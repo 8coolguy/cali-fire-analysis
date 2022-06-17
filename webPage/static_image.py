ce0a58a890e339461978a84f1aa481da9a7bd4c4
Arnav Choudhury <66573183+8coolguy@users.noreply.github.com>
2022-06-16 21:59:31 -0700

    static map

--- webPage/static_image.py
+++ webPage/static_image.py
@@ -1,18 +1,22 @@
 from mapbox import Static
+from sklearn.cluster import KMeans
 import pandas as pd
 import os
 
 df=pd.read_csv("data/new_fire_weather_data.csv")
 service =Static(access_token=os.getenv('MAPBOX'))
-fires=[]
-for i in range(30):
-    print(f'{round(float(df.iloc[i].incident_latitude),2)}--{round(float(df.iloc[i].incident_longitude),2)}')
-    fires.append({
+points =[[round(float(df.iloc[i].incident_longitude),5),round(float(df.iloc[i].incident_latitude),5)] for i in range(df.shape[0])]
+km=KMeans(n_clusters=20)
+km.fit(points)
+fires=km.cluster_centers_
+points=[]
+for i in range(len(fires)):
+    points.append({
         'type': 'Feature',
-        'properties':{'name':'fire'},
+        'properties':{'marker-size':'small'},
         'geometry': {
             'type': 'Point',
-            'coordinates': [round(float(df.iloc[i].incident_longitude),2),round(float(df.iloc[i].incident_latitude),2)]}
+            'coordinates': [round(float(fires[i][0]),2),round(float(fires[i][-1]),2)]}
         })
 # >>> bend = {
 # ...    'type': 'Feature',
@@ -20,7 +24,7 @@ for i in range(30):
 # ...    'geometry': {
 # ...        'type': 'Point',
 # ...        'coordinates': [-121.3153, 44.0582]}}
-response = service.image('mapbox.satellite',z=5 ,lon=-119.2667,lat=37.1137,width=324,height=394,features=fires)
+response = service.image('mapbox.satellite',z=5 ,lon=-119,lat=37,width=320,height=394,features=points)
 print(response.status_code)
 with open('/tmp/map.png', 'wb') as output:
     _ = output.write(response.content)
